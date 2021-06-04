7bb4b65fa81c9d141b08a9e0e09125cc37338d1e
davidgraper <davidgraper@gmail.com>
2021-06-04 09:47:08 -0400

    Added backupexecutor.py file

--- main.py
+++ main.py
@@ -1,34 +1,106 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-bashCommand = "du /home/dgraper/colossus_share0/Work-related/ -d 2 | sort -k 2"
-
 import subprocess
 import csv
+import datetime
 
-process = subprocess.Popen(['du', '/home/dgraper/colossus_share0/Movies', '-d', '2' ], stdout=subprocess.PIPE)
-stdout, stderr = process.communicate()
+def getdirectories2rsync(paths):
 
-reader = csv.DictReader(stdout.decode('ascii').splitlines(),
-                        delimiter='\t', skipinitialspace=True,
-                        fieldnames=['space', 'name'])
+    lists = []
+    for path in paths:
 
-dict1 = {}
+        print("Processing {0}".format(path))
 
-for row in reader:
-    dict1.items.
-    print(row)
+        process = subprocess.Popen(['du', path, '-d', '1' ], stdout=subprocess.PIPE)
+        stdout, stderr = process.communicate()
 
-print(sorted(reader, key='name'))
+        reader = csv.reader(stdout.decode('ascii').splitlines(),
+                        delimiter='\t', quotechar="'")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        list1 = []
+
+        # isFirstRow = True
+        for row in reader:
+            if not(row[1] == path):
+                list1.append((row[1], row[0]))
+            # else:
+            #     isFirstRow = False
+
+        list1.sort()
+        lists.append(list1)
+
+        # For each list, read the entry and add it to a csv file
+        storagesum = 0
+        blockindicator = 0
+
+        csvfile = open('directory_breakdown.csv', 'w', newline='')
+        directory_breakdown = csv.writer(csvfile)
+
+        for list2 in lists:
+            for member in list2:
+                directoryname = member[0]
+                directorysize = int(member[1])
+
+                try:
+
+                # du returns size in K, so terabyte is adjusted downwards 1K
+                # 8T = 8E9
+                # 500G = 5E8
+                # 465.76 = 4.65E8
+                # 240G = 2.4E8
+                # 223G = 2.2357E8 <= Correct size?
+
+                    if (storagesum + directorysize) > 4.65E8:
+                        print("465GB Breakpoint")
+                        print("Storagesum = {0}, Directorysize = {1}. Directoryname = {2}".format(storagesum, directorysize,directoryname))
+                        storagesum = directorysize
+                        blockindicator += 1
+                    else:
+                        storagesum += directorysize
+
+                    # print("{0}\t{1}\t({2}\t{3})".format(str(blockindicator), directoryname, directorysize, str(storagesum)))
+                    directory_breakdown.writerow([blockindicator, directoryname, directorysize, storagesum])
+                except:
+                    print("error")
+
+        print("Done")
+
+# Get paths to backup -- mind the capitalization!!
+paths2backup = [# '/home/dgraper/colossus_share0/Video',
+                # '/home/dgraper/colossus_share0/Work-related',
+                '/home/dgraper/colossus_share0/Movies',
+                # '/home/dgraper/colossus_share0/Television',
+                # '/home/dgraper/colossus_share0/House-related',
+                # '/home/dgraper/colossus_share0/Audio/Music',
+                ]
+
+# Create work scheduling file
+getdirectories2rsync(paths2backup)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#
+
+# # print("Printing list1")
+# #
+# # list1.sort(reverse=True)
+# #
+# # print(list1)
+#
+# def print_hi(name):
+#
+#     # Use a breakpoint in the code line below to debug your script.
+#
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+#
+#
+#
+# # Press the green button in the gutter to run the script.
+#
+# if __name__ == '__main__':
+#
+#     print_hi('PyCharm')
+#
+#
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
