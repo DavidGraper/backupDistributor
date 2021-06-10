251f2832fb8946689d4105c30c118223b83b0056
davidgraper <davidgraper@gmail.com>
2021-06-10 07:34:01 -0400

    Added backupexecutor.py file

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 import subprocess
 import csv
 import datetime
+import argparse
+from os.path import exists
+import sys
 
-def getdirectories2rsync(paths):
+
+dryrun = False
+pathstobackupfile = ""
+outputfilename = ""
+directorysearchdepth = 0
+backupdevicesize = 0
+
+
+def getdirectories2rsync(directorysearchdepth, pathstobackup, outputfilename, backupdevicesize):
 
     lists = []
-    for path in paths:
+    for path in pathstobackup:
 
         print("Processing {0}".format(path))
 
-        process = subprocess.Popen(['du', path, '-d', '1' ], stdout=subprocess.PIPE)
+        # Run Linux "DU" command against path
+        process = subprocess.Popen(['du', path, '-d', str(directorysearchdepth) ], stdout=subprocess.PIPE)
         stdout, stderr = process.communicate()
 
+        # Parse "DU" output as CSV-compliant lines
         reader = csv.reader(stdout.decode('ascii').splitlines(),
                         delimiter='\t', quotechar="'")
 
+        # Add sorted list of directories into master list
         list1 = []
-
-        # isFirstRow = True
         for row in reader:
             if not(row[1] == path):
                 list1.append((row[1], row[0]))
-            # else:
-            #     isFirstRow = False
 
         list1.sort()
         lists.append(list1)
@@ -31,7 +41,8 @@ def getdirectories2rsync(paths):
         storagesum = 0
         blockindicator = 0
 
-        csvfile = open('directory_breakdown.csv', 'w', newline='')
+        # csvfile = open('directory_breakdown.csv', 'w', newline='')
+        csvfile = open(outputfilename, 'w', newline='')
         directory_breakdown = csv.writer(csvfile)
 
         for list2 in lists:
@@ -48,59 +59,76 @@ def getdirectories2rsync(paths):
                 # 240G = 2.4E8
                 # 223G = 2.2357E8 <= Correct size?
 
-                    if (storagesum + directorysize) > 4.65E8:
-                        print("465GB Breakpoint")
-                        print("Storagesum = {0}, Directorysize = {1}. Directoryname = {2}".format(storagesum, directorysize,directoryname))
+                    if (storagesum + directorysize) > float(backupdevicesize):
                         storagesum = directorysize
                         blockindicator += 1
                     else:
                         storagesum += directorysize
 
-                    # print("{0}\t{1}\t({2}\t{3})".format(str(blockindicator), directoryname, directorysize, str(storagesum)))
                     directory_breakdown.writerow([blockindicator, directoryname, directorysize, storagesum])
-                except:
+                except e:
                     print("error")
 
-        print("Done")
 
-# Get paths to backup -- mind the capitalization!!
-paths2backup = [# '/home/dgraper/colossus_share0/Video',
-                # '/home/dgraper/colossus_share0/Work-related',
-                '/home/dgraper/colossus_share0/Movies',
-                # '/home/dgraper/colossus_share0/Television',
-                # '/home/dgraper/colossus_share0/House-related',
-                # '/home/dgraper/colossus_share0/Audio/Music',
-                ]
+def readpathstobackupfromfile(filename):
+    if exists(filename):
+        file = open(filename ,mode='r')
 
-# Create work scheduling file
-getdirectories2rsync(paths2backup)
+        returnlist = []
+        for line in file:
+            returnlist.append(line.rstrip())
 
+        file.close()
+    else:
+        raise Exception("Backup file cannot be found")
 
+    return returnlist
 
 #
+# Start
+#
+if __name__ == '__main__':
 
-# # print("Printing list1")
-# #
-# # list1.sort(reverse=True)
-# #
-# # print(list1)
-#
-# def print_hi(name):
-#
-#     # Use a breakpoint in the code line below to debug your script.
-#
-#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#
-#
-#
-#
-#
-# # Press the green button in the gutter to run the script.
-#
-# if __name__ == '__main__':
-#
-#     print_hi('PyCharm')
-#
-#
-#
-# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # create parser
+    parser = argparse.ArgumentParser(add_help=True)
+
+    # add arguments to the parser
+    parser.add_argument("--pathstobackup", help="Input filename with source paths to back up")
+    parser.add_argument("--outputfile", help="Output filename to store size/names of directories in the specified source paths")
+    parser.add_argument("--searchdepth", help="Depth that source paths will be searched")
+    parser.add_argument("--backupdevicesize", help="Size of backup devices (exponential number okay)")
+    parser.add_argument("--dryrun", action="store_true")
+
+    # parse the arguments
+    try:
+        args = parser.parse_args()
+    except:
+        print("error 1")
+
+    dryrun = args.dryrun
+    directorysearchdepth = args.searchdepth
+    pathstobackupfile = args.pathstobackup
+    outputfilename = args.outputfile
+    backupdevicesize = args.backupdevicesize
+
+    # Get list of paths in backup file
+    try:
+        pathstobackup = readpathstobackupfromfile(pathstobackupfile)
+    except Exception as e:
+        print("Cannot read paths to backup: {0}".format(e))
+        exit()
+
+    if dryrun:
+        print("Summary")
+        print("=======")
+        print("Program will get sizes and names of all directories under these paths:")
+        for path in pathstobackup:
+            print("\t{0}".format(path))
+        print("\n* Output will be stored in file: '{0}'".format(outputfilename))
+        print("* Directory search depth set at: {0}".format(directorysearchdepth))
+        print("* Agent distribution will be based on a backup device size of:  {0}".format(backupdevicesize))
+        exit()
+
+    getdirectories2rsync(directorysearchdepth, pathstobackup, outputfilename, backupdevicesize)
+
+    print("Done.")
